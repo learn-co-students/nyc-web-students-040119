86a1a41b1175f6655bc773bfc041cbe3dfd85d78
github username <github email address>
2019-04-23 16:52:34 -0400

    added patch and delete after review

--- 15-sinatra-REST/rappers/app/controllers/rapper_controller.rb
+++ 15-sinatra-REST/rappers/app/controllers/rapper_controller.rb
@@ -20,6 +20,7 @@ class RapperController < ApplicationController
 
 
   post "/rappers" do
+
     @rapper = Rapper.create(params)
     redirect "/rappers/#{@rapper.id}"
   end
@@ -30,8 +31,26 @@ class RapperController < ApplicationController
     #   - form should populate with the current rapper's information
     # - form needs to submit to a new controller action
 
+    get "/rappers/:id/edit" do
+      @rapper = Rapper.find(params[:id])
+      erb :"rappers/edit"
+    end
+
+    patch "/rappers/:id" do
+      @rapper = Rapper.find(params[:id])
+      #update rapper's name
+      @rapper.update(name: params[:name])
+      erb :"rappers/show"
+    end
+
   #For Delete:
     # - Straight forward delete action/route/resource that finds the rapper in question and deletes that rapper from the DB
+
+    delete "/rappers/:id" do
+      Rapper.find(params[:id]).destroy
+      redirect "/rappers"
+    end
+
 
 
 end
--- 15-sinatra-REST/rappers/config.ru
+++ 15-sinatra-REST/rappers/config.ru
@@ -5,4 +5,5 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 run ApplicationController
+use Rack::MethodOverride
 use RapperController
