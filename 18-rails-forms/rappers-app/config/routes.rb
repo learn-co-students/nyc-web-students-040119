daca4b1659e2a17603b5c341df1e3e12d34e4b48
github username <github email address>
2019-04-26 10:47:24 -0400

    changes

--- 18-rails-forms/rappers-app/config/routes.rb
+++ 18-rails-forms/rappers-app/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'rappers/new'
   # get 'rappers/edit'
 
-  # get '/rappers/swag', to: "rappers#swag"
+  get '/rappers/swag', to: "rappers#swag"
   resources :rappers, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
