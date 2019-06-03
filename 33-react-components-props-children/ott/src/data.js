06caf56d45f9613d19a01d6d986b9c10da179199
Tim Campbell <timothylevicampbell@gmail.com>
2019-06-03 09:12:11 -0400

    Initial commit

---
+++ 33-react-components-props-children/ott/src/data.js
@@ -0,0 +1,60 @@
+module.exports = {
+  current_user: {
+    username: "Galilawyn",
+    profile_picture: "",
+    active_channel: "#rivers"
+  },
+  channels: [
+    {
+      name: "#pups-only",
+      messages: []
+    },
+    {
+      name: "#feeding",
+      messages: []
+    },
+    {
+      name: "#history",
+      messages: []
+    },
+    {
+      name: "#rivers",
+      messages: [
+        {
+          user: {
+            username: "Asaynnon",
+            profile_picture: ""
+          },
+          message: {
+            sent: "some-date",
+            text: "Lorem ipsum dolor sit amet"
+          }
+        },
+        {
+          user: {
+            username: "Galilawyn",
+            profile_picture: ""
+          },
+          message: {
+            sent: "some-date",
+            text: "Quam euismod sem, semper ut"
+          }
+        },
+        {
+          user: {
+            username: "Asaynnon",
+            profile_picture: ""
+          },
+          message: {
+            sent: "some-date",
+            text: "Lorem ipsum dolor sit amet"
+          }
+        }
+      ]
+    },
+    {
+      name: "#grooming",
+      messages: []
+    }
+  ]
+};
