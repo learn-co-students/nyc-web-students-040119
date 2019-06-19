a2030f18b2acd62e2ee8c64b2cc175812614b3b1
Tim Campbell <timothylevicampbell@gmail.com>
2019-06-19 10:30:22 -0400

    Add updated code

--- 40-redux-intro/src/index.js
+++ 40-redux-intro/src/index.js
@@ -4,25 +4,81 @@ import "./index.css";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { createStore } from 'redux';
+
+const defaultState = { count: 0 }
+
+function reducer(state = defaultState, action) {
+  switch (action.type) {
+    case "INCREMENT_COUNT":
+      // state = {count: 1}
+
+      return {
+        ...state,
+        count: state.count + 1
+      }
+      break;
+    case "DECREMENT_COUNT":
+
+      return {
+        ...state,
+        count: state.count - 1
+      }
+
+      break;
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+store.dispatch({ type: 'INCREMENT_COUNT' })
+store.dispatch({ type: 'INCREMENT_COUNT' })
+store.dispatch({ type: 'INCREMENT_COUNT' }) // count: 3
+console.log(store.getState());
+store.dispatch({ type: 'DECREMENT_COUNT' }) // count: 2
+console.log(store.getState());
+
+/**
+
+App
+  Header
+  Counter
+
+Thinking in Redux:
+  * Figure out state; state will be global; just an object
+    { count: 0 }
+  * Create a store to hold the state
+    * Have to pass a reducer (function that calls setState) to the createStore
+    * Has three useful functions for us:
+      * `getState()`... just does what it says
+      * `dispatch()` this (in combo with the reducer) will be like setState
+
+
+**/
+
+
+
 class App extends Component {
-  state = { count: 0 };
+  // state = { count: 0 };
 
   increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
+    // this.setState(prevState => ({ count: prevState.count + 1 }));
   };
 
   decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
+    // this.setState(prevState => ({ count: prevState.count - 1 }));
   };
 
   render() {
     return (
       <div className="App">
-        <Header count={this.state.count} />
+        <Header count={null/*this.state.count*/} />
         <Counter
-          count={this.state.count}
-          increment={this.increment}
-          decrement={this.decrement}
+          count={null/*this.state.count*/}
+          increment={null/*this.increment*/}
+          decrement={null/*this.decrement*/}
         />
       </div>
     );
